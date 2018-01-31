6878652d3bd1e0d150b5554aa9e229cabc64b762
Brian Stern <briansternhk@gmail.com>
2018-01-31 15:38:04 -0500

    passing properties

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>React App</h1>
+        <p>Here's a line!</p>
+        <Person name="Brian" age="28"/>
+        <Person name="Kev" age="26"/>
+        <Person name="Julie" age ="27" >My Hobbies: Ballet</Person>
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
