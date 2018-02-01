80d4779af528359b8d4e187469f2848697ec4a2f
Brian Stern <briansternhk@gmail.com>
2018-01-31 22:35:22 -0500

    manipulating state

--- src/App.js
+++ src/App.js
@@ -3,14 +3,35 @@ import './App.css';
 import Person from './Person/Person.js';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Brian', age: 28 },
+      { name: 'Kev', age: 26 },
+      { name: 'Manu', age: 29 }
+    ]
+  }
+
+  switchNameHandler = () => {
+    // console.log('clicked');
+    // DON'T DO THIS: this.state.persons[0].name = 'BRIAN';
+    this.setState( {
+      persons: [
+        { name: 'Bri', age: 28 },
+        { name: 'Kevin', age: 26 },
+        { name: 'Manu', age: 36 }
+      ]
+    } )
+  }
+
   render() {
     return (
       <div className="App">
         <h1>React App</h1>
         <p>Here's a line!</p>
-        <Person name="Brian" age="28"/>
-        <Person name="Kev" age="26"/>
-        <Person name="Julie" age ="27" >My Hobbies: Ballet</Person>
+        <button onClick={this.switchNameHandler}>Switch name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age ={this.state.persons[2].age} >My Hobbies: Soccer</Person>
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
