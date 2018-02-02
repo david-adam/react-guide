e6f431d7275c423524419b0e9c279cdff6f59a48
Brian Stern <briansternhk@gmail.com>
2018-02-02 17:19:29 -0500

    flexy list components

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Person from './Person/Person.js';
 class App extends Component {
   state = {
     persons: [
-      { name: 'Brian', age: 28 },
-      { name: 'Kev', age: 26 },
-      { name: 'Manu', age: 29 }
+      { id: '1', name: 'Brian', age: 28 },
+      { id: '2', name: 'Kev', age: 26 },
+      { id: '3', name: 'Manu', age: 29 }
     ]
   }
 
@@ -24,14 +24,30 @@ class App extends Component {
     } )
   }
 
-  nameChangedHandler = (event) => {
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
     this.setState( {
-      persons: [
-        { name: 'Brian', age: 28 },
-        { name: event.target.value, age: 26 },
-        { name: 'Manu', age: 36 }
-      ]
+      persons: persons
     } )
+  }
+
+  deletePersonHandler = (personIndex) => {
+    // const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
   }
 
   togglePersonsHandler = () => {
@@ -55,10 +71,13 @@ class App extends Component {
     if ( this.state.showPersons ) {
       persons = (
         <div>
-          {this.state.persons.map(person => {
+          {this.state.persons.map((person, index) => {
             return <Person
+              click={() => this.deletePersonHandler(index)}
               name={person.name}
-              age={person.age} />
+              age={person.age}
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)} />
           })}
         </div>
       )
