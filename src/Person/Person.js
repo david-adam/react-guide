f308fafdb6384fd958e784ca772268546fc06932
Brian Stern <briansternhk@gmail.com>
2018-02-09 15:57:17 -0500

    remove error boundaries

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -5,10 +5,6 @@ const person = (props) => {
 
   const rnd = Math.random();
 
-  if (rnd > 0.7) {
-    throw new Error('something went wrong');
-  }
-
   return (
     <div className={classes.Person}>
       <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
