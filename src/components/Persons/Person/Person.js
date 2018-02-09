3e20262ee5a594dc3af0bf4ff2d2c59de4edc5be
Brian Stern <briansternhk@gmail.com>
2018-02-09 17:01:17 -0500

    split app into components

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -3,8 +3,6 @@ import classes from './Person.css';
 
 const person = (props) => {
 
-  const rnd = Math.random();
-
   return (
     <div className={classes.Person}>
       <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
