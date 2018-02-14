a1e24346fe8d71487f58e021ef8e1ec78a84cf58
Brian Stern <briansternhk@gmail.com>
2018-02-14 18:43:01 -0500

    performance gains with PureComponent instead of shouldComponentUpdate method

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
-class App extends Component {
+class App extends PureComponent {
   constructor(props) {
     super(props);
     console.log('[App.js] Inside Constructor', props);
@@ -18,10 +18,11 @@ class App extends Component {
     console.log('[App.js] Inside componentDidMount()');
   }
 
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log('[UPDATE App.js] Inside shouldComponentUpdate', nextProps, nextState);
-    return true;
-  }
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   console.log('[UPDATE App.js] Inside shouldComponentUpdate', nextProps, nextState);
+  //   return nextState.persons !== this.state.persons ||
+  //     nextState.showPersons !== this.state.showPersons;
+  // }
 
   componentWillUpdate(nextProps, nextState) {
     console.log('UPDATE [App.js] Inside componentWillUpdate', nextProps, nextState);
@@ -86,6 +87,10 @@ class App extends Component {
     } )
   }
 
+  showPersonsHandler = () => {
+    this.setState({showPersons: true})
+  }
+
   render() {
 
     console.log('[App.js] Inside render()')
@@ -101,6 +106,7 @@ class App extends Component {
 
     return (
         <div className={classes.App}>
+          <button onClick={this.showPersonsHandler}>Show Persons</button>
           <Cockpit
             appTitle={this.props.title}
             showPersons={this.state.showPersons}
