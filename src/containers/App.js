a2c5a95750f64af421e22f85e83acced2148bedd
Brian Stern <briansternhk@gmail.com>
2018-02-19 18:26:21 -0500

    understanding higher order components

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import React, { PureComponent } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
+import WithClass from '../hoc/WithClass';
 
 class App extends PureComponent {
   constructor(props) {
@@ -105,7 +106,7 @@ class App extends PureComponent {
     }
 
     return (
-        <div className={classes.App}>
+      <WithClass classes={classes.App}>
           <button onClick={this.showPersonsHandler}>Show Persons</button>
           <Cockpit
             appTitle={this.props.title}
@@ -113,7 +114,7 @@ class App extends PureComponent {
             persons={this.state.persons}
             clicked={this.togglePersonsHandler} />
           {persons}
-        </div>
+      </WithClass>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
