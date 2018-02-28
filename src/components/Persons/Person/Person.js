8671f7ae5d4a64c0a3c2bec093d49b7809b63ca2
Brian Stern <brian@brickworksoftware.com>
2018-02-28 11:58:18 -0500

    validating props

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import classes from './Person.css';
 import WithClass from '../../../hoc/WithClass'
@@ -29,5 +30,12 @@ class Person extends Component {
     )
   }
 }
+
+Person.propTypes = {
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  changed: PropTypes.func
+};
 
 export default Person;
